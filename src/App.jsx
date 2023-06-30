23ea11d6ced7e4cbe71416afd11d02c93f404138
benjmatthies <benjmatthies@zohomail.com>
2023-06-29 20:34:16 -0600

    After figuring out rendering puppy details

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,32 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  let featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+  //console.log(puppies)
+
+  function handleClick(puppy) {
+     setFeatPupId(puppy.id)
+  }
   return (
     <>
       <div>
         {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          }
-          )
+          puppies.map((puppy) => 
+          {
+            return <p onClick={()=>handleClick(puppy)} 
+              key={puppy.id}>{puppy.name}</p>
+          })
         }
+        {featPupId && 
+          (<div>                                                          
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>)}
       </div>
     </>
   )
